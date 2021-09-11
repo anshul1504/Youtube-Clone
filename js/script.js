8f7ba90578b9f9fbe0e2cff86dfb27971cae558e
thewebfix <thewebfix1504@gmail.com>
2021-09-11 15:05:53 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var menuIcon = document.querySelector(".menu");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
